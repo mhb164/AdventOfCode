981078371a4004f0811c085ab1d45c863271f232
mhb164 <mhb164@gmail.com>
2021-12-14 21:52:04 +0330

    Solve Day03

--- AdventOfCode2021/PuzzleStep.cs
+++ AdventOfCode2021/PuzzleStep.cs
@@ -75,6 +75,7 @@ namespace AdventOfCode2021
             var output = new List<PuzzleStep>();
             output.Add(new PuzzleStep(01, Day01_1SolveMethod, Day01_2SolveMethod, LoadTestInput("Day01TestInput"), "7", "5", "Sonar Sweep"));
             output.Add(new PuzzleStep(02, Day02_1SolveMethod, Day02_2SolveMethod, LoadTestInput("Day02TestInput"), "150", "900", "Dive!"));
+            output.Add(new PuzzleStep(03, Day03_1SolveMethod, Day03_2SolveMethod, LoadTestInput("Day03TestInput"), "198", "230", "Binary Diagnostic"));
             return output;
         }
 
@@ -172,11 +173,94 @@ namespace AdventOfCode2021
                 {
                     aim -= int.Parse(line.Replace("up", ""));
                 }
-                
+
             }
             return (forwardTotal * depth).ToString();
         }
         #endregion
 
+        #region Day03
+        private static string Day03_1SolveMethod(IEnumerable<string> lines)
+        {
+            var HighCounts = new List<int>();
+            foreach (var line in lines)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (HighCounts.Count <= i) HighCounts.Add(0);
+                    if (line[i] == '1') HighCounts[i]++;
+                }
+            }
+
+            var gammaRate = "";
+            var epsilonRate = "";
+            for (int i = 0; i < HighCounts.Count; i++)
+            {
+                if (HighCounts[i] > lines.Count() - HighCounts[i]) { gammaRate += "1"; epsilonRate += "0"; }
+                else { gammaRate += "0"; epsilonRate += "1"; }
+            }
+
+
+            var gammaRateDigit = Convert.ToInt32(gammaRate, 2);
+            var epsilonRateDigit = Convert.ToInt32(epsilonRate, 2);
+
+            return (gammaRateDigit * epsilonRateDigit).ToString();
+
+        }
+        private static string Day03_2SolveMethod(IEnumerable<string> lines)
+        {
+            if (lines.Count() == 0) return "";
+            var maxlength = lines.First().Trim().Length;
+            var miror = lines.ToList();
+            var col = 0;
+            while (col > maxlength || miror.Count > 1)
+            {
+                var mostCommon = GetMostCommon(miror, col);
+                miror.RemoveAll(x => x.Trim()[col] != mostCommon);
+                col++;
+            }
+            var oxygenRating = Convert.ToInt32(miror[0], 2);
+
+
+            miror = lines.ToList();
+            col = 0;
+            while (col > maxlength || miror.Count > 1)
+            {
+                var mostCommon = GetMostCommon(miror, col);
+                miror.RemoveAll(x => x.Trim()[col] == mostCommon);
+                col++;
+            }
+            var CO2Rating = Convert.ToInt32(miror[0], 2);
+
+
+            return (oxygenRating * CO2Rating).ToString();
+
+        }
+
+        private static char GetMostCommon(List<string> items, int col)
+        {
+            var count0 = 0;
+            var count1 = 0;
+            foreach (var item in items)
+            {
+                if (item.Trim()[col] == '0') count0++;
+                else count1++;
+            }
+            if (count1 >= count0) return '1';
+            else return '0';
+        }
+        #endregion
+
+        //#region DayXX
+        //private static string DayXX_1SolveMethod(IEnumerable<string> lines)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        //private static string DayXX_2SolveMethod(IEnumerable<string> lines)
+        //{
+        //    throw new NotImplementedException();
+
+        //}
+        //#endregion
     }
 }
